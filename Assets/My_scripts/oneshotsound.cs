88750894d99095d4a301ced0ee663d27449a45e1
ISTIAKPAPPU <istiak1021@gmail.com>
2020-04-10 21:11:03 +0600

    Sounds

---
+++ Assets/My_scripts/oneshotsound.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oneshotsound : MonoBehaviour
+{
+    [SerializeField]
+    GameObject sound;
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            sound.gameObject.SetActive(true);
+            Destroy(gameObject, 7);
+        }
+    }
+}
